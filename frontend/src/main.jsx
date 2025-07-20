6967490b5dc4769fe888583c18ce40da2008b95e
Gcvishwas <gcvishwas17@gmail.com>
2025-07-20 17:20:07 +0545

    Configured routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,47 @@ import {
   RouterProvider,
 } from "react-router";
 import './index.css'
-import App from './App.jsx'
+import HomePage from './routes/Home/HomePage';
+import SignInPage from './routes/SignIn/SignInPage';
+import SignUpPage from './routes/SignUp/SignUpPage';
+import DashboardPage from './routes/Dashboard/DashboardPage';
+import ChatPage from './routes/Chat/ChatPage';
+import RootLayout from './layouts/RootLayout/RootLayout';
+import DashboardLayout from './layouts/DashboardLayout/DashboardLayout';
+
 const router = createBrowserRouter([
+  {element:<RootLayout/>,
+children:[
+  
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <HomePage/>,
   },
+  {
+    path: "/sign-in/*",
+    element: <SignInPage/>,
+  },
+  {
+    path: "/sign-up/*",
+    element: <SignUpPage/>,
+  },
+  {
+    element:<DashboardLayout/>,
+    children:[
+      
+      
+      {
+        path: "/dashboard",
+        element: <DashboardPage/>,
+      },
+      {
+        path: "/dashboard/chats/:id",
+        element: <ChatPage/>,
+      },
+    ]
+  }
+]
+}
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
