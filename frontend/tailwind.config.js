67d03d325b2bc4803d5cc7398cc4b21619de92b2
Gcvishwas <gcvishwas17@gmail.com>
2025-07-22 17:02:29 +0545

    Add animations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    keyframes: {
-      rotateOrbital: {
-        "0%": { transform: "rotate(0deg)" },
-        "100%": { transform: "rotate(360deg)" },
+    extend: {
+      animation: {
+        rotateOrbital: "rotateOrbital 100s linear infinite",
+        ambulanceAnimate: "botAnimate 2s linear infinite alternate",
+        slideBg: "slideBg 8s ease-in-out infinite alternate",
       },
-    },
-    animation: {
-      rotateOrbital: "rotateOrbital 200s linear infinte",
+      keyframes: {
+        rotateOrbital: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        ambulanceAnimate: {
+          "0%": { transform: "scale(1)" },
+          "100%": { transform: "scale(1.1)" },
+        },
+        slideBg: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
     },
   },
   plugins: [],
