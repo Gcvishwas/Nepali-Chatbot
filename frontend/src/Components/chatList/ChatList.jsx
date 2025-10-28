22567025701056a4fe4fc2a224b5755c5bac94f5
Gcvishwas <gcvishwas17@gmail.com>
2025-10-28 15:34:13 +0545

    Changed routes

--- frontend/src/Components/chatList/ChatList.jsx
+++ frontend/src/Components/chatList/ChatList.jsx
@@ -65,8 +65,8 @@ const ChatList = () => {
     <div className="flex flex-col h-full">
       <span className="title font-semibold text-[10px] mb-2.5">DASHBOARD</span>
       <Link to="/dashboard" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">Create a new Chat</Link>
-      <Link to="/" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">Explore</Link>
-      <Link to="/" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">Contact</Link>
+      <Link to="/explore" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">Explore</Link>
+      <Link to="/emergency" className="p-2.5 rounded-[10px] hover:bg-[#2c2937]">Emergency Contacts</Link>
 
       <hr className="border-none h-[2px] bg-[#ddd] opacity-10 rounded-[5px] my-5" />
       
